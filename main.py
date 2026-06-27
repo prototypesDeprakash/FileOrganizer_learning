5dfc6c5f53da186434fef5bf9799a553543338b6
Prakash <prakashmvp8080@gmail.com>
2026-06-27 18:12:02 +0530

    created the basic working

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 import os
-
+import shutil
 class FileOrganizer:
 
     
 
     def __init__(self):
-        self.userDirectory="H:\ISO"
+        self.userDirectory= r"H:\ISO\Project_testOnly"
         self.allFiles=[]
 
         #Different file Types
@@ -15,14 +15,14 @@ class FileOrganizer:
         self.zipFiles=[]
         self.exeFiles=[]
         self.xlsxFiles=[]
-        self.exeFiles=[]
+        
         self.txtFiles=[]
         self.unitypackage=[]
         
         self.Folders=[]
 
         self.FolderNaming = ["PDF_Files","Image_Files","MP3_Files","ZIP_Files","EXE_Files",
-                             "Excel_Files","Text_Files","UnityPacages","Text_Files","User_Folders"]
+                             "Excel_Files","UnityPackages","Text_Files","User_Folders"]
 
 
     #program flow manager
@@ -31,7 +31,10 @@ class FileOrganizer:
         self.print_directory()
         self.scan_userDirectory()
         self.categorize_Files()
-        self.print_allcategory()
+        #self.print_allcategory()
+        self.create_Folders()
+        self.move_FilesToFolders()
+        self.move_FolderstoFolders()
 
 
     #function to get directory details from user
@@ -66,12 +69,11 @@ class FileOrganizer:
             
             #for folder categorizing
             else:
-                #ignoring already creaded folder
-                if(current_file in self.FolderNaming):
-                    pass
-                #if not our folder then catogorize it
-                else:
-                    self.Folders.append(current_file)
+                full_path = os.path.join(self.userDirectory, current_file)
+
+                if os.path.isdir(full_path):
+                    if current_file not in self.FolderNaming:
+                        self.Folders.append(current_file)
                     
 
     def create_Folders(self):
@@ -79,10 +81,64 @@ class FileOrganizer:
 
         for folderName in self.FolderNaming:
             if(folderName not in self.allFiles):
-                os.mkdir(folderName)
+                final_folder_path = os.path.join(self.userDirectory,folderName)
+                os.mkdir(final_folder_path)
 
     def move_FilesToFolders(self):
-        pass
+        #move pdf files
+        for pdf in self.pdfFiles:
+            source_path = os.path.join(self.userDirectory,pdf)
+            destination_path = os.path.join(self.userDirectory,"PDF_Files")
+            shutil.move(source_path,destination_path)
+        #move image files
+        for image in self.imageFiles:
+            source_path = os.path.join(self.userDirectory,image)
+            destination_path = os.path.join(self.userDirectory,"Image_Files")
+            shutil.move(source_path,destination_path)
+        
+        #move MP3_Files
+        for mp3 in self.mp3Files:
+            source_path = os.path.join(self.userDirectory,mp3)
+            destination_path = os.path.join(self.userDirectory,"MP3_Files")
+            shutil.move(source_path,destination_path)
+
+
+        #move ZIP_Files
+        for zip in self.zipFiles:
+            source_path = os.path.join(self.userDirectory,zip)
+            destination_path = os.path.join(self.userDirectory,"ZIP_Files")
+            shutil.move(source_path,destination_path)
+
+        #move EXE_Files
+        for exe in self.exeFiles:
+            source_path = os.path.join(self.userDirectory,exe)
+            destination_path = os.path.join(self.userDirectory,"EXE_Files")
+            shutil.move(source_path,destination_path)
+
+        #move Excel_Files
+        for excel in self.xlsxFiles:
+            source_path = os.path.join(self.userDirectory,excel)
+            destination_path = os.path.join(self.userDirectory,"Excel_Files")
+            shutil.move(source_path,destination_path)
+
+        #move UnityPacages
+        for unityPack in self.unitypackage:
+            source_path = os.path.join(self.userDirectory,unityPack)
+            destination_path = os.path.join(self.userDirectory,"UnityPackages")
+            shutil.move(source_path,destination_path)
+
+        #move Text_Files
+        for text in self.txtFiles:
+            source_path = os.path.join(self.userDirectory,text)
+            destination_path = os.path.join(self.userDirectory,"Text_Files")
+            shutil.move(source_path,destination_path)
+
+    def move_FolderstoFolders(self):
+        #move User_Folders
+        for folder in self.Folders:
+            source_path = os.path.join(self.userDirectory,folder)
+            destination_path = os.path.join(self.userDirectory,"User_Folders")
+            shutil.move(source_path,destination_path)
 
     #test functions
     def print_directory(self):
