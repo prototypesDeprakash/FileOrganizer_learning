399424927ea0dfd927b47c717af035bd1d737b32
Prakash <prakashmvp8080@gmail.com>
2026-06-27 16:44:18 +0530

    categorizationDoneSuccessfully

--- main.py
+++ main.py
@@ -18,7 +18,11 @@ class FileOrganizer:
         self.exeFiles=[]
         self.txtFiles=[]
         self.unitypackage=[]
+        
         self.Folders=[]
+
+        self.FolderNaming = ["PDF_Files","Image_Files","MP3_Files","ZIP_Files","EXE_Files",
+                             "Excel_Files","Text_Files","UnityPacages","Text_Files","User_Folders"]
 
 
     #program flow manager
@@ -26,6 +30,8 @@ class FileOrganizer:
         #self.get_directory()
         self.print_directory()
         self.scan_userDirectory()
+        self.categorize_Files()
+        self.print_allcategory()
 
 
     #function to get directory details from user
@@ -35,17 +41,84 @@ class FileOrganizer:
     #scan all files in that directory
     def scan_userDirectory(self):
         self.allFiles = os.listdir(self.userDirectory)
-        for i in self.allFiles:
-            print(i)
+        
+
+    def categorize_Files(self):
+        for current_file in self.allFiles:
+            if(current_file[-3::] =="pdf"):
+                self.pdfFiles.append(current_file)
+            elif(current_file[-3::]=="png"):
+                self.imageFiles.append(current_file)
+            elif(current_file[-3::]=="jpg"):
+                self.imageFiles.append(current_file)
+            elif(current_file[-3::]=="zip"):
+                self.zipFiles.append(current_file)
+            elif(current_file[-3::]=="txt"):
+                self.txtFiles.append(current_file)
+            elif(current_file[-3::]=="mp3"):
+                self.mp3Files.append(current_file)
+            elif(current_file[-3::]=="exe"):
+                self.exeFiles.append(current_file)
+            elif(current_file[-4::]=="xlsx"):
+                self.xlsxFiles.append(current_file)
+            elif(current_file[-12::]=="unitypackage"):
+                self.unitypackage.append(current_file)
+            
+            #for folder categorizing
+            else:
+                #ignoring already creaded folder
+                if(current_file in self.FolderNaming):
+                    pass
+                #if not our folder then catogorize it
+                else:
+                    self.Folders.append(current_file)
+                    
+            
 
 
+        
 
+    def create_Folders(self):
+        #custom folder naming:
+        '''
+        pdf fies = PDF_Files
+        
+        '''
+        pass
 
-
+    def move_FilesToFolders(self):
+        pass
 
     #test functions
     def print_directory(self):
         print(self.userDirectory)
+    def print_allcategory(self):
+        print("--------folders--------")
+        print(self.Folders)
+        print("--------images--------")
+        print(self.imageFiles)
+        print("--------pdfs--------")
+        print(self.pdfFiles)
+        print("--------exeFiles--------")
+        print(self.exeFiles)
+        print("--------zipfies--------")
+        print(self.zipFiles)
+        print("--------Excelfiles--------")
+        print(self.xlsxFiles)
+        print("--------UnityPackages--------")
+        print(self.unitypackage)
+        print("--------mp3 Files--------")
+        print(self.mp3Files)
+        print("--------text files--------")
+        print(self.txtFiles)
+
+
+
+
+
+
+
+
     
     
 
--- osunderstanding.py
+++ osunderstanding.py
@@ -1,6 +1,10 @@
 import os
 
 
-file_lsit= os.listdir("H:\ISO")
-for i in file_lsit:
-    print(i)
+# file_lsit= os.listdir("H:\ISO")
+# for i in file_lsit:
+#     print(i)
+
+
+testname = "hellow.unitypackage"
+print(testname[-12::])
